e361d832c30d6647035e1d5f09bde009e426f262
mahmoud BOUATE <12065@holbertonschool.com>
2025-12-17 15:35:04 +0100

    task 3

--- funct.c
+++ funct.c
@@ -1,11 +1,13 @@
 #include "shell.h"
 
+/* Affiche le prompt */
 void print_prompt(int interactive)
 {
     if (interactive)
         write(STDOUT_FILENO, PROMPT, strlen(PROMPT));
 }
 
+/* Lecture de l'entrée utilisateur */
 ssize_t read_input(char **line, size_t *len, int interactive)
 {
     ssize_t nread = getline(line, len, stdin);
@@ -23,6 +25,7 @@ ssize_t read_input(char **line, size_t *len, int interactive)
     return nread;
 }
 
+/* Supprime espaces début et fin */
 char *trim(char *str)
 {
     char *end;
@@ -41,6 +44,7 @@ char *trim(char *str)
     return str;
 }
 
+/* Découpe la ligne en arguments */
 void split_line(char *line, char **argv)
 {
     int i = 0;
@@ -54,13 +58,59 @@ void split_line(char *line, char **argv)
     argv[i] = NULL;
 }
 
+/* Recherche la commande dans le PATH */
+char *find_in_path(char *command)
+{
+    char *path, *path_copy, *dir;
+    static char full_path[1024];
+
+    if (strchr(command, '/'))
+    {
+        if (access(command, X_OK) == 0)
+            return command;
+        return NULL;
+    }
+
+    path = getenv("PATH");
+    if (!path)
+        return NULL;
+
+    path_copy = strdup(path);
+    if (!path_copy)
+        return NULL;
+
+    dir = strtok(path_copy, ":");
+    while (dir)
+    {
+        snprintf(full_path, sizeof(full_path), "%s/%s", dir, command);
+        if (access(full_path, X_OK) == 0)
+        {
+            free(path_copy);
+            return full_path;
+        }
+        dir = strtok(NULL, ":");
+    }
+
+    free(path_copy);
+    return NULL;
+}
+
+/* Exécution de la commande */
 void execute_command(char **argv)
 {
     pid_t pid;
     int status;
+    char *cmd_path;
 
     if (!argv[0])
         return;
+
+    cmd_path = find_in_path(argv[0]);
+    if (!cmd_path)
+    {
+        fprintf(stderr, "%s: not found\n", argv[0]);
+        return; /* PAS DE fork si commande introuvable */
+    }
 
     pid = fork();
     if (pid == -1)
@@ -68,11 +118,9 @@ void execute_command(char **argv)
 
     if (pid == 0)
     {
-        if (execve(argv[0], argv, environ) == -1)
-        {
-            perror(argv[0]);
-            exit(EXIT_FAILURE);
-        }
+        execve(cmd_path, argv, environ);
+        perror(argv[0]);
+        exit(EXIT_FAILURE);
     }
     else
     {
