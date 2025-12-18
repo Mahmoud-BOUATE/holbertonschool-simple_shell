72f446dfee3d4c32f77bf9a54bfb71dc8452ff0c
mahmoud BOUATE <12065@holbertonschool.com>
2025-12-18 11:37:55 +0100

    test

--- task_5.c
+++ task_5.c
@@ -1,18 +1,11 @@
-/* task_5.c */
-/* Implémente le built-in exit pour Simple Shell 0.4 */
 
 #include <stdlib.h>
 #include <string.h>
 
-/**
- * handle_exit - gère la commande exit
- * @argv: tableau d'arguments
- *
- * Si argv[0] est "exit", quitte le shell.
- */
+/* built-in exit */
 int handle_exit(char **argv)
 {
     if (argv[0] && strcmp(argv[0], "exit") == 0)
-        exit(0);
-    return 0;
+        exit(0);  /* quitte immédiatement le shell avec status 0 */
+    return 0;     /* sinon rien */
 }
