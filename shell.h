88c66d05d5b77159951b6f0b7acb41d79831e1e4
mahmoud BOUATE <12065@holbertonschool.com>
2025-12-18 11:41:06 +0100

    add prototype in header

--- shell.h
+++ shell.h
@@ -18,5 +18,6 @@ void split_line(char *line, char **argv);
 ssize_t read_input(char **line, size_t *len, int interactive);
 void execute_command(char **argv);
 void print_prompt(int interactive);
+int handle_exit(char **argv);
 
 #endif
